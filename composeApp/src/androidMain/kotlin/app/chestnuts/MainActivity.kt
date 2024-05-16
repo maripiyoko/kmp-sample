535950006e68e8c5f5518b1eade2ab470dbf6831
maripiyo <maripiyok@gmail.com>
2024-05-16 19:05:05 +0900

    AppList

--- composeApp/src/androidMain/kotlin/app/chestnuts/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/app/chestnuts/MainActivity.kt
@@ -1,9 +1,10 @@
 package app.chestnuts
 
-import App
+import AppList
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -11,8 +12,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
+
         setContent {
-            App()
+            AppList()
         }
     }
 }
@@ -20,5 +23,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    AppList()
 }
